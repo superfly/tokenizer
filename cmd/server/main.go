4f74c72d4e2f01a41b9d1cb49d746586db287b8d
Ben Toews <benjamin.toews@gmail.com>
2023-04-28 13:33:50 -0600

    disable debugging

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,7 +9,6 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"github.com/sirupsen/logrus"
 	"github.com/superfly/tokenizer"
@@ -40,7 +39,7 @@ func main() {
 		logrus.WithError(err).Fatal("listen")
 	}
 
-	l = debugListener{l}
+	// l = debugListener{l}
 
 	tkz := tokenizer.NewTokenizer(os.Getenv("OPEN_KEY"))
 	tkz.ProxyHttpServer.Verbose = true
@@ -81,55 +80,55 @@ func handleSignals(server *http.Server) {
 	}
 }
 
-type debugListener struct {
-	net.Listener
-}
+// type debugListener struct {
+// 	net.Listener
+// }
 
-func (dl debugListener) Accept() (net.Conn, error) {
-	c, err := dl.Listener.Accept()
-	if err == nil {
-		c = debugConn{c}
-	}
-	return c, err
-}
+// func (dl debugListener) Accept() (net.Conn, error) {
+// 	c, err := dl.Listener.Accept()
+// 	if err == nil {
+// 		c = debugConn{c}
+// 	}
+// 	return c, err
+// }
 
-type debugConn struct {
-	c net.Conn
-}
+// type debugConn struct {
+// 	c net.Conn
+// }
 
-func (dc debugConn) Read(b []byte) (int, error) {
-	n, err := dc.c.Read(b)
-	if err == nil {
-		fmt.Printf("<- %#v\n", string(b[:n]))
-	}
-	return n, err
-}
+// func (dc debugConn) Read(b []byte) (int, error) {
+// 	n, err := dc.c.Read(b)
+// 	if err == nil {
+// 		fmt.Printf("<- %#v\n", string(b[:n]))
+// 	}
+// 	return n, err
+// }
 
-func (dc debugConn) Write(b []byte) (int, error) {
-	fmt.Printf("-> %#v\n", string(b))
-	return dc.c.Write(b)
-}
+// func (dc debugConn) Write(b []byte) (int, error) {
+// 	fmt.Printf("-> %#v\n", string(b))
+// 	return dc.c.Write(b)
+// }
 
-func (dc debugConn) Close() error {
-	return dc.c.Close()
-}
+// func (dc debugConn) Close() error {
+// 	return dc.c.Close()
+// }
 
-func (dc debugConn) LocalAddr() net.Addr {
-	return dc.c.LocalAddr()
-}
+// func (dc debugConn) LocalAddr() net.Addr {
+// 	return dc.c.LocalAddr()
+// }
 
-func (dc debugConn) RemoteAddr() net.Addr {
-	return dc.c.RemoteAddr()
-}
+// func (dc debugConn) RemoteAddr() net.Addr {
+// 	return dc.c.RemoteAddr()
+// }
 
-func (dc debugConn) SetDeadline(t time.Time) error {
-	return dc.c.SetDeadline(t)
-}
+// func (dc debugConn) SetDeadline(t time.Time) error {
+// 	return dc.c.SetDeadline(t)
+// }
 
-func (dc debugConn) SetReadDeadline(t time.Time) error {
-	return dc.c.SetReadDeadline(t)
-}
+// func (dc debugConn) SetReadDeadline(t time.Time) error {
+// 	return dc.c.SetReadDeadline(t)
+// }
 
-func (dc debugConn) SetWriteDeadline(t time.Time) error {
-	return dc.c.SetWriteDeadline(t)
-}
+// func (dc debugConn) SetWriteDeadline(t time.Time) error {
+// 	return dc.c.SetWriteDeadline(t)
+// }
