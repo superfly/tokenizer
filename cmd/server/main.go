9f17cdbf17f8be5426224b9d098b1426d320091e
Ben Toews <benjamin.toews@gmail.com>
2023-07-04 09:38:14 -0600

    better logging

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -39,12 +39,12 @@ func main() {
 		logrus.WithError(err).Fatal("listen")
 	}
 
-	// l = debugListener{l}
-
 	tkz := tokenizer.NewTokenizer(os.Getenv("OPEN_KEY"))
-	tkz.ProxyHttpServer.Verbose = true
 
-	server := &http.Server{Addr: addr, Handler: tkz}
+	server := &http.Server{
+		Addr:    addr,
+		Handler: loggingMiddleware(tkz),
+	}
 
 	go func() {
 		if err := server.Serve(l); !errors.Is(err, http.ErrServerClosed) {
@@ -79,56 +79,3 @@ func handleSignals(server *http.Server) {
 		}
 	}
 }
-
-// type debugListener struct {
-// 	net.Listener
-// }
-
-// func (dl debugListener) Accept() (net.Conn, error) {
-// 	c, err := dl.Listener.Accept()
-// 	if err == nil {
-// 		c = debugConn{c}
-// 	}
-// 	return c, err
-// }
-
-// type debugConn struct {
-// 	c net.Conn
-// }
-
-// func (dc debugConn) Read(b []byte) (int, error) {
-// 	n, err := dc.c.Read(b)
-// 	if err == nil {
-// 		fmt.Printf("<- %#v\n", string(b[:n]))
-// 	}
-// 	return n, err
-// }
-
-// func (dc debugConn) Write(b []byte) (int, error) {
-// 	fmt.Printf("-> %#v\n", string(b))
-// 	return dc.c.Write(b)
-// }
-
-// func (dc debugConn) Close() error {
-// 	return dc.c.Close()
-// }
-
-// func (dc debugConn) LocalAddr() net.Addr {
-// 	return dc.c.LocalAddr()
-// }
-
-// func (dc debugConn) RemoteAddr() net.Addr {
-// 	return dc.c.RemoteAddr()
-// }
-
-// func (dc debugConn) SetDeadline(t time.Time) error {
-// 	return dc.c.SetDeadline(t)
-// }
-
-// func (dc debugConn) SetReadDeadline(t time.Time) error {
-// 	return dc.c.SetReadDeadline(t)
-// }
-
-// func (dc debugConn) SetWriteDeadline(t time.Time) error {
-// 	return dc.c.SetWriteDeadline(t)
-// }
