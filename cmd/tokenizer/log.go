c001cc8a8fb5a7ce56fd0df292dd3f84d575737a
Ben Toews <1144197+btoews@users.noreply.github.com>
2023-09-21 13:28:04 -0600

    better proxying on fly.io (#15)
    The "http" handler was causing headaches by rewriting the request URI.
    It turns out, doing TCP passthrough solves this entirely, removing the
    need for some hacky workarounds. Tokenizer now plays more nicely with
    curl.

--- cmd/tokenizer/log.go
+++ cmd/tokenizer/log.go
@@ -9,23 +9,32 @@ import (
 	"golang.org/x/exp/maps"
 )
 
-type statusCodeRecorder struct {
+type responseWriter interface {
 	http.ResponseWriter
+	http.Hijacker
+}
+
+type statusCodeRecorder struct {
+	responseWriter
 	statusCode int
 }
 
 func (w *statusCodeRecorder) WriteHeader(statusCode int) {
 	w.statusCode = statusCode
-	w.ResponseWriter.WriteHeader(statusCode)
+	w.responseWriter.WriteHeader(statusCode)
 }
 
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		scw := &statusCodeRecorder{ResponseWriter: w}
+		scw := &statusCodeRecorder{responseWriter: w.(responseWriter)}
 		queryKeys := strings.Join(maps.Keys(r.URL.Query()), ", ")
 		start := time.Now()
 
 		next.ServeHTTP(scw, r)
+
+		if scw.statusCode == 0 {
+			scw.statusCode = http.StatusOK
+		}
 
 		logrus.WithFields(logrus.Fields{
 			"method":    r.Method,
