713235c1ab5e855bd953d2fdfac4227c745fe239
Tim <165851289+timflyio@users.noreply.github.com>
2025-11-20 08:03:19 -1000

    support sealing an arbitrary json tokenizer secret from the command line (#32)
    * support sealing an arbitrary json tokenizer secret from the command line
    * update golang version and crypto library to appease vuln scan checks

--- cmd/sealtoken/main.go
+++ cmd/sealtoken/main.go
@@ -40,9 +40,27 @@ func wrapToken(token, sealKey, orgSlug, appSlug, targHost, targHdr string, debug
 	return secret.Seal(sealKey)
 }
 
+func wrapJsonToken(token, sealKey string, debug bool) (string, error) {
+	var secret tokenizer.Secret
+	if err := json.Unmarshal([]byte(token), &secret); err != nil {
+		return "", fmt.Errorf("json.Unmarshal: %w", err)
+	}
+
+	if debug {
+		bs, err := json.Marshal(secret)
+		if err != nil {
+			return "", fmt.Errorf("json.Marshal: %w", err)
+		}
+		return string(bs), nil
+	}
+
+	return secret.Seal(sealKey)
+}
+
 func tryMain() error {
 	defSealKey := os.Getenv("SEAL_KEY")
 	sealKey := flag.String("sealkey", defSealKey, "tokenizer seal key, or from environment SEAL_KEY")
+	json := flag.Bool("json", false, "create sealed token from json tokenizer secret")
 	orgSlug := flag.String("org", "", "allowed org slug")
 	appSlug := flag.String("app", "", "allowed app slug")
 	targHost := flag.String("host", "", "target host")
@@ -62,23 +80,33 @@ func tryMain() error {
 	if *sealKey == "" {
 		return fmt.Errorf("sealkey unspecified")
 	}
-	if *orgSlug == "" {
-		return fmt.Errorf("org unspecified")
-	}
-	if *appSlug == "" {
-		return fmt.Errorf("app unspecified")
-	}
-	if *targHost == "" {
-		return fmt.Errorf("target host unspecified")
-	}
 
-	token := args[0]
+	if *json {
+		j := args[0]
+		wrapped, err := wrapJsonToken(j, *sealKey, *debug)
+		if err != nil {
+			return fmt.Errorf("wrapJsonToken: %w", err)
+		}
+		fmt.Printf("%s\n", wrapped)
+	} else {
+		if *orgSlug == "" {
+			return fmt.Errorf("org unspecified")
+		}
+		if *appSlug == "" {
+			return fmt.Errorf("app unspecified")
+		}
+		if *targHost == "" {
+			return fmt.Errorf("target host unspecified")
+		}
 
-	wrapped, err := wrapToken(token, *sealKey, *orgSlug, *appSlug, *targHost, *targHdr, *debug)
-	if err != nil {
-		return fmt.Errorf("wrapToken: %w", err)
+		token := args[0]
+
+		wrapped, err := wrapToken(token, *sealKey, *orgSlug, *appSlug, *targHost, *targHdr, *debug)
+		if err != nil {
+			return fmt.Errorf("wrapToken: %w", err)
+		}
+		fmt.Printf("%s\n", wrapped)
 	}
-	fmt.Printf("%s\n", wrapped)
 	return nil
 }
 
